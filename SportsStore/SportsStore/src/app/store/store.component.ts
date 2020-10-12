5a12631cdc06dd9a3e331f0c711c5ab2724b7275
Patil2 <SPatil@ad.harman.com>
2020-10-12 15:37:00 +0530

    pagination exmple

---
+++ SportsStore/SportsStore/src/app/store/store.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { Product } from '../model/product.model';
+import { ProductRepository } from '../model/product.repository';
+
+@Component({
+  selector: 'store',
+  templateUrl: './store.component.html',
+  styleUrls: ['./store.component.css']
+})
+export class StoreComponent {
+
+  public selectedCategory: string = null;
+  public productsPerPage = 4;
+  public selectedPage = 1;
+
+  constructor(private repository: ProductRepository) { }
+
+  getProducts(): Product[] {
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage
+    return this.repository.getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
+  }
+
+  getCategories(): String[] {
+    return this.repository.getCategories();
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory = newCategory;
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  getPageNumbers(): number[] {
+    return Array(Math.ceil(this.repository
+      .getProducts(this.selectedCategory).length / this.productsPerPage))
+      .fill(0).map((x, i) => i + 1);
+  }
+
+}
