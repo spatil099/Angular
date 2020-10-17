89d75df3b6f7d482e8b8e00a677e916e074af66d
Patil2 <SPatil@ad.harman.com>
2020-10-17 12:59:40 +0530

    Rest Web service application

--- SportsStore/SportsStore/src/app/model/model.module.ts
+++ SportsStore/SportsStore/src/app/model/model.module.ts
@@ -4,9 +4,13 @@ import { StaticDataSource } from "./static.datasource";
 import { Cart } from "./cart.model";
 import { OrderRepository } from "./order.repository";
 import { Order } from "./order.model";
+import { RestDataSource } from "./rest.datasource";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-    providers: [ProductRepository, StaticDataSource, Cart,Order, OrderRepository]
+    imports : [HttpClientModule],
+    providers: [ProductRepository, StaticDataSource, Cart,Order, OrderRepository,
+        { provide: StaticDataSource, useClass: RestDataSource }]
 })
 
 export class ModelModule { }
